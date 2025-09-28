bef69d1cc7a913c45ef99d68cbbced948a53592d
Muhammed Taha Kızıkoğlu <tkizikoglu10@gmail.com>
2025-09-28 15:15:41 +0300

    son Commit

--- courses/urls.py
+++ courses/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
-    path('list',views.kurslar),
     path('<kurs_adi>',views.details),
     path('kategori/<int:category_id>',views.getCoursesByCategoryId),
     path('kategori/<str:category_name>',views.getCoursesByCategory, name ='courses_by_category')
+    # name parametresi -> Kodda URL’yi doğrudan string olarak yazmak yerine bu ismi kullanmaktır.
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,22 +1,51 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from datetime import date
 
 data = {"programlama":"programlama kategorisine ait kurslar",
         "web-geliştirme":"web-geliştirme kategorisine ait kurslar",
-        "mobil geliştirme ":"mobil  geliştirme kategorisine ait kurslar"
+        "mobil-geliştirme":"mobil geliştirme kategorisine ait kurslar"
 }
-def index(request):
-    return render(request,'courses/index.html')
 
-def kurslar(request):
-    return HttpResponse("Kurslar")
+db = {
+    "courses": [
+        {
+            "title":"javascript kursu",
+            "description":"javascript kursunun açıklamasıdır.",
+            "imageUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuHnJDLOcdm_0b6N6kNj-1OvO9KhKYgqIy0w&s",
+            "slug":"javascript-kursu",
+            "date": date(2022,10,10),
+            "is-active":True
+        },
+         {
+            "title":"python kursu",
+            "description":"python kursunun açıklamasıdır.",
+            "imageUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuHnJDLOcdm_0b6N6kNj-1OvO9KhKYgqIy0w&s",
+            "slug":"python-kursu",
+            "date": date(2023,10,10),
+            "is-active":False
+        },
+        {
+            "title":"web geliştirme kursu",
+            "description":"web kursunun açıklamasıdır.",
+            "imageUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuHnJDLOcdm_0b6N6kNj-1OvO9KhKYgqIy0w&s",
+            "slug":"web-geliştirme-kursu",
+            "date": date(2024,10,10),
+            "is-active":True
+        }]
+}
+
+def index(request):
+    category_list = list(data.keys())
+    #html'ye liste şeklinde action'dan 3. parametre kullanarak veri yolladık -> categories değişkeni ile
+    return render(request,'courses/index.html',{'categories': category_list})
 
 def details(request,kurs_adi):
     return HttpResponse(f"{kurs_adi} kursunun detay sayfası")
 
 def getCoursesByCategory(request,category_name):   
-    try:
+    try:    
         category_text = data[category_name]
         return render(request,"courses/kurslar.html",{
             'category':category_name,
@@ -32,7 +61,7 @@ def getCoursesByCategoryId(request,category_id):
     
     category = category_list[category_id - 1]
 
+    # url patternin nameini yeniden yepılandırır, args-> dinamik kısımları sırayla atar
     redirect_url = reverse('courses_by_category',args=[category])
     
     return redirect(redirect_url)
-    
